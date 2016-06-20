5efffcaeb45f5ea491636106cc5828b4e0b59153
Wayne Bishop <bishopwb66@gmail.com>
2016-06-21 11:00:32 +1200

    Added stream functionality

--- app/controllers/StreamController.php
+++ app/controllers/StreamController.php
@@ -7,6 +7,10 @@ class StreamController extends PageController {
 
 	// Constructor
 	public function __construct($dbc) {
+		
+		// Run the parent constructor
+		parent::__construct();
+
 		$this->dbc = $dbc;
 
 		// If you are not logged in
@@ -18,6 +22,32 @@ class StreamController extends PageController {
 
 	// Methods (functions)
 	public function buildHTML() {
+
+		// Get latest posts (pins)
+		$allData = $this->getLatestPosts();
+
+		$data = [];
+
+		$data['allPosts'] = $allData;
+
+		echo $this->plates->render('stream', $data);
+
+	}
+
+	private function getLatestPosts() {
+
+		// Prepare some SQL
+		$sql = "SELECT * 
+				FROM posts";
+
+		// Run the SQL and capture the result
+		$result = $this->dbc->query($sql);	
+
+		// Extract the resuLts as an associative array per (MYSQLI_ASSOC)
+		$allData = $result->fetch_all(MYSQLI_ASSOC);			
+		
+		// Return the results to the code that called this function	
+		return $allData;
 
 	}
 
